ca75903b049e2f14f70f1956e82a1f57471754d5
Federico Carrizo <nicolascarrizook@gmail.com>
2022-03-24 23:55:54 -0300

    entrega 03

--- index.js
+++ index.js
@@ -1,48 +1,30 @@
 const fs = require('fs');
-
-//crear clase que reciba el nombre del archivo
-
 class Contenedor {
     constructor(nombreArchivo) {
         this.nombreArchivo = nombreArchivo;
     }
-
-    getId() {
-        //devolver el id mas alto + 1
+    //crear metodo que reciba un objeto, lo guardo en un archivo y devuelvo el id asignado
+    save(objeto) {
         let contenido = fs.readFileSync(this.nombreArchivo, 'utf-8');
         let objetos = JSON.parse(contenido);
-        let id = objetos.length > 0 ? objetos[objetos.length - 1].id + 1 : 1;
+        objetos.push(JSON.stringify(objeto));
+        let id = Date.now()
+        fs.writeFileSync(this.nombreArchivo, objetos);
         return id;
     }
-
-
-    // recibe un objeto, lo guarda en el archivo, devuelve el id asignado
-
-    save(objeto) {
-        //guardar el objeto en el archivo
-        let id = this.getId();
-        objeto.id = id;
-        let json = JSON.stringify(objeto);
-        fs.writeFileSync(this.nombreArchivo, json + '\n');
-        return id;
-    }
-
-   
     //crear metodo que reciba un id y devuelva el objeto correspondiente o null si no existe
     findById(id) {
         let contenido = fs.readFileSync(this.nombreArchivo, 'utf-8');
-        let objetos = JSON.parse(contenido);
+        let objetos = [JSON.parse(contenido)];
         let objeto = objetos.find(obj => obj.id == id);
         return objeto;
     }
-
     //devolver un array con todos los objetos del archivo
     findAll() {
         let contenido = fs.readFileSync(this.nombreArchivo, 'utf-8');
         let objetos = JSON.parse(contenido);
         return objetos;
     }
-
     //eliminar del archivo el objeto con el id recibido
     delete(id) {
         let contenido = fs.readFileSync(this.nombreArchivo, 'utf-8');
@@ -52,27 +34,51 @@ class Contenedor {
         objetos.splice(indice, 1);
         fs.writeFileSync(this.nombreArchivo, JSON.stringify(objetos));
     }
-
     //elimina todos los objetos del archivo
     deleteAll() {
         fs.writeFileSync(this.nombreArchivo, '[]');
     }
+    //Obtiene un producto random
+    getRandomArbitrary () {
+        let contenido = fs.readFileSync(this.nombreArchivo, 'utf-8');
+        const lista = JSON.parse(contenido);
+        const index = (Math.random() * (lista.length - 1)).toFixed();
+        console.log(index)
+        const object = lista[index]
+        return object
+    }
 }
 
-// instanciar la clase
-let contenedor = new Contenedor('productos.txt');
-//metodo save
-let prod1 = contenedor.save({ nombre: 'Leche', precio: 100 });
-console.log(prod1);
+const express = require('express')
+const PORT = 8080;
+const app = express();
 
-let prod2 = contenedor.save({ nombre: 'Arroz', precio: 200 });
-console.log(prod2);
+app.get('/', (req,res) => {
+    res.send(`<h1 style="color: blue;">Desafio 3</h1>`)
+})
+app.get('/productos', (req,res) => {
+    try {
+        const nombre = 'productos.txt'
+        const archivo = new Contenedor (nombre)
+        const list = JSON.stringify(archivo.findAll())
+        res.send(`<p>${list}</p>`)
+    }
+    catch {
+        res.send(`<p>${[]}</p>`)
+    }
+})
+app.get('/productoRandom', (req,res) => {
+    try {
+        const nombre = 'productos.txt'
+        const archivo = new Contenedor (nombre)
+        res.send(`<p>${JSON.stringify(archivo.getRandomArbitrary())}</p>`)
+    }
+    catch {
+        res.send(`<p>${[]}</p>`)
+    }
+})
 
-
-//metodo findById
-let prod3 = contenedor.findById(prod1);
-console.log(prod3);
-
-
-
-
+const server = app.listen(PORT, () => {
+    console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
+})
+server.on("error", error => console.error(`Error en servidor ${error}`))
